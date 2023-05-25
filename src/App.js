ec360923d44aca7f9ed1efdd3d915522110d8096
MonguDykrai <40307989+MonguDykrai@users.noreply.github.com>
2023-05-25 11:58:45 +0800

    支持拖拽播放，重复提示，列表渲染播放器取url播放

--- src/App.js
+++ src/App.js
@@ -20,6 +20,26 @@ export default function App() {
           <button onClick={() => setType(16)}>16</button>
         </li>
       </ul>
+      <ol>
+        <li
+          draggable="true"
+          data-url="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+          onDragStart={(event) => {
+            event.dataTransfer.setData('text/plain', event.target.dataset.url);
+          }}
+        >
+          地址1
+        </li>
+        <li
+          draggable="true"
+          data-url="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+          onDragStart={(event) => {
+            event.dataTransfer.setData('text/plain', event.target.dataset.url);
+          }}
+        >
+          地址2
+        </li>
+      </ol>
       <div className="container">
         <Stage type={type} />
       </div>
--- src/Stage.js
+++ src/Stage.js
@@ -1,36 +1,77 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import classNames from 'classnames';
 import './style.css';
 
 const Stage = ({ type }) => {
+  const previousType = useRef(type);
+  const [screens, setScreens] = useState([
+    { key: 1, url: '' },
+    { key: 2, url: '' },
+    { key: 3, url: '' },
+    { key: 4, url: '' },
+    { key: 5, url: '' },
+    { key: 6, url: '' },
+    { key: 7, url: '' },
+    { key: 8, url: '' },
+    { key: 9, url: '' },
+    { key: 10, url: '' },
+    { key: 11, url: '' },
+    { key: 12, url: '' },
+    { key: 13, url: '' },
+    { key: 14, url: '' },
+    { key: 15, url: '' },
+    { key: 16, url: '' },
+  ]);
+  useEffect(() => {
+    if (type !== previousType.current) {
+      if (type < previousType.current) {
+        setScreens((prev) => {
+          prev.forEach((screen) => {
+            // 假定：上一次选择(previousType.current)的类型为9，本次选择(type)的类型为4
+            // 那么 screen.key 值大于 type 的元素的 url 都得置为空字符串（模拟移除摄像头）
+            if (screen.key > type) {
+              screen.url = '';
+            }
+          });
+          return prev.map((screen) => screen);
+        });
+      }
+    }
+    previousType.current = type;
+  }, [type]);
   return (
     <>
       <div className={classNames('stage', `stage-${type}`)}>
-        <div>
-          <video
-            src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-            style={{ width: '100%', height: '100%' }}
-            controls
-            autoplay
-            muted
-          />
-        </div>
-        {/* <div>screen 1</div> */}
-        <div>screen 2</div>
-        <div>screen 3</div>
-        <div>screen 4</div>
-        <div>screen 5</div>
-        <div>screen 6</div>
-        <div>screen 7</div>
-        <div>screen 8</div>
-        <div>screen 9</div>
-        <div>screen 10</div>
-        <div>screen 11</div>
-        <div>screen 12</div>
-        <div>screen 13</div>
-        <div>screen 14</div>
-        <div>screen 15</div>
-        <div>screen 16</div>
+        {screens.map((screen) => (
+          <div
+            key={screen.key}
+            onDragOver={(event) => {
+              event.preventDefault();
+            }}
+            onDrop={(event) => {
+              const currentScreen = screens.find(
+                (scre) => scre.key === screen.key
+              );
+              if (currentScreen && currentScreen.url) {
+                window.alert('当前区域已有设备播放！');
+                return;
+              }
+              var url = event.dataTransfer.getData('text');
+              setScreens((prev) => {
+                prev[screen.key - 1].url = url;
+                return prev.map((screen) => screen);
+              });
+            }}
+          >
+            <video
+              src={screen.url}
+              style={{ width: '100%', height: '100%' }}
+              controls
+              autoPlay
+              muted
+            />
+          </div>
+        ))}
       </div>
     </>
   );
